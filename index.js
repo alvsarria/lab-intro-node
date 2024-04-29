bc734eb275aebe74a061a776a20995766e52a420
Alvaro Sarria <alvaro.sarria.rico@gmail.com>
2024-04-29 16:06:01 +0200

    solved lab

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  };
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  };
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] === undefined) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    };
+  };
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.sort((a, b) => b - a)[0];
+    };
+  };
 
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    };
+  };
+  
+  sum() {
+    return this.items.reduce((acc, cur) => acc + cur, 0);
+  };
+  
+  avg() { 
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items.reduce((acc, cur) => acc + cur, 0)/this.items.length;
+      // return this.items.sum()/this.items.length;
+    };
+  };
 }
 
 module.exports = SortedList;
